47afe3a0c9dc7bee60340d6eae6ec560dd73b669
Hulkingpluto <2486072@students.wits.ac.za>
2024-10-06 17:43:44 +0200

    minor fixes

--- Client-servers/register.js
+++ Client-servers/register.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://backendapi2003.azurewebsites.net/api';
+const API_BASE_URL = 'https://backend2077.azurewebsites.net/api';
 
 //handle clicking the show password eye
 // Get references to the password input and the toggle icon
