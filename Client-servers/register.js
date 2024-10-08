4138386de6915278ba399ea56115730fee96a93e
Hulkingpluto <2486072@students.wits.ac.za>
2024-10-08 08:28:32 +0200

    minor fixes

--- Client-servers/register.js
+++ Client-servers/register.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://witscampustutoring.azurewebsites.net/api';
 
 //handle clicking the show password eye
 // Get references to the password input and the toggle icon
