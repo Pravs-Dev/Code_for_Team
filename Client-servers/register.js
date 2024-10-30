787485d91c6a826f21911ea014a780934e6ab08e
Pravs-Dev <pravirstudy@gmail.com>
2024-10-30 12:44:26 +0200

    Update register.js

--- Client-servers/register.js
+++ Client-servers/register.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://witscampustutoring.azurewebsites.net/api';
+const API_BASE_URL = 'https://testingdeploy1.azurewebsites.net/api';
 
 //handle clicking the show password eye
 // Get references to the password input and the toggle icon
